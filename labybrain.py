7e743db836a51d600d1591d565071aa6a6e86e6c
kseen <korenevdn@gmail.com>
2023-07-30 06:11:07 +0300

    Almost production ready

--- labybrain.py
+++ labybrain.py
@@ -1,23 +1,27 @@
-import json
-import tensorflow as tf
-from tensorflow import keras
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
 from logger import LB_Logger
+from tqdm.keras import TqdmCallback
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+import tensorflow as tf
+import json
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 class Global_vars:
     config = {
         "randomness": 0.5,
-        "direction_1": 0.0,
-        "direction_2": 0.0,
-        "direction_3": 0.0,
-        "direction_4": 0.0,
-        "direction_5": 0.0,
-        "direction_6": 0.0,
+        "dir_1": 0.0,
+        "dir_2": 0.0,
+        "dir_3": 0.0,
+        "dir_4": 0.0,
+        "dir_5": 0.0,
+        "dir_6": 0.0,
     }
+    model = keras.Sequential()
+    model_filename = ""
 
 
 global_vars = Global_vars()
@@ -58,17 +62,69 @@ def import_train_data(filename: str):
         # chop off the last column and save it as labels
         train_labels = train_data.pop('result')
         # convert every label to a one-hot vector
+        train_labels = train_labels - 1
         train_labels = tf.one_hot(train_labels, depth=6)
         return train_data, train_labels
     except Exception as e:
         lb_logger.log_error("Failed to import train data: " + str(e))
 
 
+def load_model(model_filename: str):
+    '''
+    Loads the model from model.keras.
+    '''
+    try:
+        global_vars.model = keras.models.load_model(model_filename)
+        lb_logger.log_info("Model " + model_filename + " loaded.")
+        return global_vars.model
+    except Exception as e:
+        lb_logger.log_error("Failed to load model: " + str(e))
+
+
+def predict(predict_pd: pd.DataFrame):
+    '''
+    Predicts the result based on config.
+    '''
+    prediction = global_vars.model.predict(predict_pd)[0]
+    max_prediction = max(prediction)
+    # prediction_id = np.where(prediction, max(prediction))[0] + 1
+    prediction_id = np.where(np.isclose(prediction, max(prediction)))[0] + 1
+    # if prediction_id is list:
+    prediction_id = prediction_id[0]
+    return prediction, max_prediction, prediction_id
+    # ('MDL: ' + global_vars.model_filename + '\n' +
+    #         'INPT:\n' +
+    #         str(predict_pd) + '\n' +
+    #         'PRDT: ' + str(prediction) + '\n' +
+    #         'RSLT: ' + str(max_prediction) + ' == ' +
+    #         str(prediction_id))
+
+
+def insert_randomity(randomness: float, predict_pd: pd.DataFrame):
+    '''
+    Inserts randomity into config data.
+    '''
+    local_pr_pd = predict_pd.copy()
+    # get random number from 0 to 1
+
+    # add random number to every dir, cap it at 0 and 1
+    for i in range(1, 7):
+        random_number = np.random.rand()
+        sign_of_random_number = np.random.choice([-1, 1])
+        random_number = sign_of_random_number * random_number * randomness
+        local_pr_pd["dir_" + str(i)] = \
+            np.clip(local_pr_pd["dir_" + str(i)] + random_number, 0, 1)
+    return local_pr_pd
+
+
 if __name__ == "__main__":
     train_data, train_labels = import_train_data("train_data.csv")
+    # lb_logger.log_info("Train data: " + str(train_data))
+    # lb_logger.log_info("Train labels: " + str(train_labels))
 
     model = keras.Sequential([
-        # keras.layers.Dense(6, activation='relu'),
+        keras.layers.Dense(6, activation='relu'),
+        keras.layers.Dense(3, activation='relu'),
         keras.layers.Dense(6, activation='sigmoid'),
         # keras.layers.Dense(6)
     ])
@@ -76,44 +132,59 @@ if __name__ == "__main__":
     model.compile(optimizer='adam',
                   loss='binary_crossentropy', metrics=['accuracy'])
 
-    model.fit(train_data, train_labels, epochs=10)
+    model.fit(train_data, train_labels, epochs=1000,
+              verbose=0, callbacks=[TqdmCallback(verbose=1)])
 
-    test_loss, test_acc = model.evaluate(train_data,  train_labels, verbose=2)
+    test_loss, test_acc = model.evaluate(train_data,  train_labels, verbose=0)
 
     lb_logger.log_info('Test accuracy: {}'.format(test_acc))
     lb_logger.log_info('Test loss: {}'.format(test_loss))
 
-    test_pd0 = pd.DataFrame([[0, 0, 0, 0, 0, 0]], columns=train_data.columns)
-    prediction = model.predict(test_pd0)[0]
-    print('0: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
-          str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
+    # save model
+    max_model_id = 0
+    for filename in os.listdir('models'):
+        if filename.startswith('model_') and filename.endswith('.keras'):
+            model_id = int(filename[6:-6])
+            if model_id > max_model_id:
+                max_model_id = model_id
+    model_id = max_model_id + 1
+    four_digit_model_id = str(model_id).zfill(4)
+    model_filename = 'models/model_' + four_digit_model_id + '.keras'
+    model.save(model_filename)
+    lb_logger.log_info('Model saved as ' + model_filename)
 
-    test_pd1 = pd.DataFrame([[1, 0, 0, 0, 0, 0]], columns=train_data.columns)
-    prediction = model.predict(test_pd1)[0]
-    print('1: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
-          str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
+    # print(predict())
+    # test_pd0 = pd.DataFrame([[0, 0, 0, 0, 0, 0]], columns=train_data.columns)
+    # prediction = model.predict(test_pd0)[0]
+    # print('0: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
+    #       str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
 
-    test_pd2 = pd.DataFrame([[0, 1, 0, 0, 0, 0]], columns=train_data.columns)
-    prediction = model.predict(test_pd2)[0]
-    print('2: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
-          str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
+    # test_pd1 = pd.DataFrame([[1, 0, 0, 0, 0, 0]], columns=train_data.columns)
+    # prediction = model.predict(test_pd1)[0]
+    # print('1: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
+    #       str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
 
-    test_pd3 = pd.DataFrame([[0, 0, 1, 0, 0, 0]], columns=train_data.columns)
-    prediction = model.predict(test_pd3)[0]
-    print('3: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
-          str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
+    # test_pd2 = pd.DataFrame([[0, 1, 0, 0, 0, 0]], columns=train_data.columns)
+    # prediction = model.predict(test_pd2)[0]
+    # print('2: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
+    #       str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
 
-    test_pd4 = pd.DataFrame([[0, 0, 0, 1, 0, 0]], columns=train_data.columns)
-    prediction = model.predict(test_pd4)[0]
-    print('4: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
-          str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
+    # test_pd3 = pd.DataFrame([[0, 0, 1, 0, 0, 0]], columns=train_data.columns)
+    # prediction = model.predict(test_pd3)[0]
+    # print('3: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
+    #       str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
 
-    test_pd5 = pd.DataFrame([[0, 0, 0, 0, 1, 0]], columns=train_data.columns)
-    prediction = model.predict(test_pd5)[0]
-    print('5: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
-          str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
+    # test_pd4 = pd.DataFrame([[0, 0, 0, 1, 0, 0]], columns=train_data.columns)
+    # prediction = model.predict(test_pd4)[0]
+    # print('4: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
+    #       str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
 
-    test_pd6 = pd.DataFrame([[0, 0, 0, 0, 0, 1]], columns=train_data.columns)
-    prediction = model.predict(test_pd6)[0]
-    print('6: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
-          str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
+    # test_pd5 = pd.DataFrame([[0, 0, 0, 0, 1, 0]], columns=train_data.columns)
+    # prediction = model.predict(test_pd5)[0]
+    # print('5: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
+    #       str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
+
+    # test_pd6 = pd.DataFrame([[0, 0, 0, 0, 0, 1]], columns=train_data.columns)
+    # prediction = model.predict(test_pd6)[0]
+    # print('6: ' + str(prediction) + '\t' + str(max(prediction)) + '\t' +
+    #       str(np.where(np.isclose(prediction, max(prediction)))[0] + 1))
--- labybrain_gui.py
+++ labybrain_gui.py
@@ -1,6 +1,7 @@
 import dearpygui.dearpygui as dpg
 import dearpygui_ext.logger as dpg_logger
 import colorama as cr
+import pandas as pd
 
 import labybrain
 from labybrain import global_vars
@@ -43,12 +44,12 @@ def save_callback():
     Saves config to config.json.
     '''
     global_vars.config["randomness"] = dpg.get_value("randomness")
-    global_vars.config["direction_1"] = dpg.get_value("direction_1")
-    global_vars.config["direction_2"] = dpg.get_value("direction_2")
-    global_vars.config["direction_3"] = dpg.get_value("direction_3")
-    global_vars.config["direction_4"] = dpg.get_value("direction_4")
-    global_vars.config["direction_5"] = dpg.get_value("direction_5")
-    global_vars.config["direction_6"] = dpg.get_value("direction_6")
+    global_vars.config["dir_1"] = dpg.get_value("dir_1")
+    global_vars.config["dir_2"] = dpg.get_value("dir_2")
+    global_vars.config["dir_3"] = dpg.get_value("dir_3")
+    global_vars.config["dir_4"] = dpg.get_value("dir_4")
+    global_vars.config["dir_5"] = dpg.get_value("dir_5")
+    global_vars.config["dir_6"] = dpg.get_value("dir_6")
     try:
         labybrain.save_config()
         logger.log("Config saved.")
@@ -62,12 +63,12 @@ def load_callback():
     '''
     labybrain.load_config()
     dpg.set_value("randomness", global_vars.config["randomness"])
-    dpg.set_value("direction_1", global_vars.config["direction_1"])
-    dpg.set_value("direction_2", global_vars.config["direction_2"])
-    dpg.set_value("direction_3", global_vars.config["direction_3"])
-    dpg.set_value("direction_4", global_vars.config["direction_4"])
-    dpg.set_value("direction_5", global_vars.config["direction_5"])
-    dpg.set_value("direction_6", global_vars.config["direction_6"])
+    dpg.set_value("dir_1", global_vars.config["dir_1"])
+    dpg.set_value("dir_2", global_vars.config["dir_2"])
+    dpg.set_value("dir_3", global_vars.config["dir_3"])
+    dpg.set_value("dir_4", global_vars.config["dir_4"])
+    dpg.set_value("dir_5", global_vars.config["dir_5"])
+    dpg.set_value("dir_6", global_vars.config["dir_6"])
     logger.log("Config loaded.")
 
 
@@ -76,12 +77,12 @@ def reset_callback():
     Resets config to default values.
     '''
     dpg.set_value("randomness", 0.5)
-    dpg.set_value("direction_1", 0.0)
-    dpg.set_value("direction_2", 0.0)
-    dpg.set_value("direction_3", 0.0)
-    dpg.set_value("direction_4", 0.0)
-    dpg.set_value("direction_5", 0.0)
-    dpg.set_value("direction_6", 0.0)
+    dpg.set_value("dir_1", 0.0)
+    dpg.set_value("dir_2", 0.0)
+    dpg.set_value("dir_3", 0.0)
+    dpg.set_value("dir_4", 0.0)
+    dpg.set_value("dir_5", 0.0)
+    dpg.set_value("dir_6", 0.0)
     logger.log("Config reset to default values.")
 
 
@@ -92,56 +93,112 @@ def dump_cfg_log():
     log = 'Dumping config to console...\n'\
         + "randomness: " \
         + str(labybrain.global_vars.config["randomness"]) + '\n'\
-        + "direction_1: " \
-        + str(labybrain.global_vars.config["direction_1"]) + '\n'\
-        + "direction_2: " \
-        + str(labybrain.global_vars.config["direction_2"]) + '\n'\
-        + "direction_3: " \
-        + str(labybrain.global_vars.config["direction_3"]) + '\n'\
-        + "direction_4: " \
-        + str(labybrain.global_vars.config["direction_4"]) + '\n'\
-        + "direction_5: " \
-        + str(labybrain.global_vars.config["direction_5"]) + '\n'\
-        + "direction_6: " \
-        + str(labybrain.global_vars.config["direction_6"])
+        + "dir_1: " \
+        + str(labybrain.global_vars.config["dir_1"]) + '\n'\
+        + "dir_2: " \
+        + str(labybrain.global_vars.config["dir_2"]) + '\n'\
+        + "dir_3: " \
+        + str(labybrain.global_vars.config["dir_3"]) + '\n'\
+        + "dir_4: " \
+        + str(labybrain.global_vars.config["dir_4"]) + '\n'\
+        + "dir_5: " \
+        + str(labybrain.global_vars.config["dir_5"]) + '\n'\
+        + "dir_6: " \
+        + str(labybrain.global_vars.config["dir_6"])
     logger.log_info(log)
     lb_logger.log_info(log)
 
 
-def pr():
-    logger.log_error('Not implemented yet!')
+def predict_callback():
+    '''
+    Predicts the result based on config.
+    '''
+    try:
+        predict_pd = pd.DataFrame([[global_vars.config["dir_1"],
+                                    global_vars.config["dir_2"],
+                                    global_vars.config["dir_3"],
+                                    global_vars.config["dir_4"],
+                                    global_vars.config["dir_5"],
+                                    global_vars.config["dir_6"]]],
+                                  columns=['dir_1', 'dir_2',
+                                           'dir_3', 'dir_4',
+                                           'dir_5', 'dir_6'])
+        load_model_id = dpg.get_value("model_id")
+        if load_model_id == "":
+            # find the latest model
+            import os
+            files = os.listdir("models")
+            files.sort()
+            load_model_id = files[-1].split("_")[1].split(".")[0]
+        global_vars.model_filename = "models/model_" + load_model_id + ".keras"
+        labybrain.load_model(global_vars.model_filename)
+        prediction, max_prediction, prediction_id = labybrain.predict(
+            labybrain.insert_randomity(global_vars.config['randomness'],
+                                       predict_pd))
+
+        prdt_str = ''
+        for i in range(len(prediction)):
+            prdt_str += str(i) + ': ' + str(prediction[i])
+            if i != len(prediction) - 1:
+                prdt_str += '\n'
+
+        logger.log_info("Predicted result for\n" +
+                        'MDL: ' + global_vars.model_filename)
+        logger.log_info('INPT:\n' + str(predict_pd))
+        logger.log_info('PRDT:\n' + prdt_str)
+        logger.log("RSLT: " + str(prediction_id) +
+                   ' (' + str(max_prediction) + ')')
+
+        labybrain.lb_logger.log_info("Predicted result for\n" +
+                                     'MDL: ' + global_vars.model_filename)
+        labybrain.lb_logger.log_info('INPT:\n' + str(predict_pd))
+        labybrain.lb_logger.log_info('PRDT:\n' + prdt_str)
+        labybrain.lb_logger.log("RSLT: " + str(prediction_id) +
+                                ' (' + str(max_prediction) + ')')
+    except Exception as e:
+        logger.log_error("Failed to predict: " + str(e))
+        labybrain.lb_logger.log_error("Failed to predict: " + str(e))
 
 
 if __name__ == "__main__":
 
     dpg.create_context()
     dpg.create_viewport(title='Labybrain', height=720, width=1280,
-                        decorated=True)
+                        decorated=True, resizable=False)
+    dpg.set_viewport_small_icon("source/favicon.ico")
+    dpg.set_viewport_large_icon("source/favicon.ico")
+
     dpg.setup_dearpygui()
 
-    with dpg.window(label="Input (krutilki)", menubar=False, width=400,
-                    height=682):
+    width, height, channels, data = dpg.load_image(
+        "source/logo.png")
+    with dpg.texture_registry():
+        texture_id = dpg.add_static_texture(width, height, data)
 
-        dpg.add_slider_float(label="Randomness", min_value=0.0,
+    with dpg.window(label="Input (krutilki)", menubar=False, width=500,
+                    height=682, no_resize=True, no_title_bar=True, no_move=True,
+                    no_collapse=True):
+
+        dpg.add_slider_float(label="randomness", min_value=0.0,
                              max_value=1.0, default_value=0, tag="randomness",
                              width=300)
-        dpg.add_slider_float(label="Direction 1", min_value=0.0,
-                             max_value=1.0, default_value=0, tag="direction_1",
+        dpg.add_slider_float(label="dir_1", min_value=0.0,
+                             max_value=1.0, default_value=0, tag="dir_1",
                              width=300)
-        dpg.add_slider_float(label="Direction 2", min_value=0.0,
-                             max_value=1.0, default_value=0, tag="direction_2",
+        dpg.add_slider_float(label="dir_2", min_value=0.0,
+                             max_value=1.0, default_value=0, tag="dir_2",
                              width=300)
-        dpg.add_slider_float(label="Direction 3", min_value=0.0,
-                             max_value=1.0, default_value=0, tag="direction_3",
+        dpg.add_slider_float(label="dir_3", min_value=0.0,
+                             max_value=1.0, default_value=0, tag="dir_3",
                              width=300)
-        dpg.add_slider_float(label="Direction 4", min_value=0.0,
-                             max_value=1.0, default_value=0, tag="direction_4",
+        dpg.add_slider_float(label="dir_4", min_value=0.0,
+                             max_value=1.0, default_value=0, tag="dir_4",
                              width=300)
-        dpg.add_slider_float(label="Direction 5", min_value=0.0,
-                             max_value=1.0, default_value=0, tag="direction_5",
+        dpg.add_slider_float(label="dir_5", min_value=0.0,
+                             max_value=1.0, default_value=0, tag="dir_5",
                              width=300)
-        dpg.add_slider_float(label="Direction 6", min_value=0.0,
-                             max_value=1.0, default_value=0, tag="direction_6",
+        dpg.add_slider_float(label="dir_6", min_value=0.0,
+                             max_value=1.0, default_value=0, tag="dir_6",
                              width=300)
 
         dpg.add_spacer(height=5)
@@ -153,13 +210,21 @@ if __name__ == "__main__":
         dpg.add_button(label="Reset", callback=reset_callback, width=300)
         dpg.add_button(label="Dump to console",
                        callback=dump_cfg_log, width=300)
+        dpg.add_spacer(height=5)
+        dpg.add_input_text(label="Model ID", readonly=False, width=300,
+                           tag="model_id",
+                           hint="Leave empty to load the latest model")
         dpg.add_button(label="Predict",
-                       callback=pr, width=300)
+                       callback=predict_callback, width=300)
+        dpg.add_image(texture_id, pos=[150, 450])
 
-    mvLogger = dpg_logger.mvLogger()
+    dpg.add_window(label="Output (log)", pos=[500, 0], menubar=False, width=765,
+                   height=682,
+                   no_resize=True, no_title_bar=True, no_move=True,
+                   no_collapse=True, tag="log_window")
+    mvLogger = dpg_logger.mvLogger(parent="log_window")
     mvLogger.log_level = 0
-    dpg.set_item_pos(mvLogger.window_id, [765, 0])
-    dpg.set_item_height(mvLogger.window_id, 682)
+
     logger = DPG_Logger(mvLogger)
 
     load_callback()
